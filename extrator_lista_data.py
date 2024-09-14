3353ef586ed95ac04c07d0d495706a63192e9e19
eduardopfbp <eduardo_palmieri@outlook.com>
2024-09-14 00:21:52 -0300

    Atualização da data para scrapp

--- extrator_lista_data.py
+++ extrator_lista_data.py
@@ -14,7 +14,7 @@ prefixo = "https://fbref.com"
 dataframes = [] 
 
 # Data de início para extração
-data_inicio = datetime.strptime('2024-09-07', '%Y-%m-%d')
+data_inicio = datetime.strptime('2024-09-14', '%Y-%m-%d')
 
 # Número máximo de tentativas falhadas
 max_falhas = 2
